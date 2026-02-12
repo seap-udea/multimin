b766b9a993e7c42a02ca0cb9d7ae00ea13b382d8
Web Developer <jorge.zuluaga@udea.edu.co>
2026-02-12 17:19:35 -0500

    [REL] released version 0.10.2

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    version='0.10.1',
+    version='0.10.2',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     test_suite="pytest",
