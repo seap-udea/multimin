c1e7d206c4fc7927f81da2e2b0b1c501e11fbcee
Web Developer <jorge.zuluaga@udea.edu.co>
2026-02-13 15:16:19 -0500

    [REL] released version 0.10.4

--- src/multimin/version.py
+++ src/multimin/version.py
@@ -6,4 +6,4 @@
 # License: GNU Affero General Public License v3 (AGPL-3.0)        #
 ##################################################################
 
-__version__ = '0.10.3'
+__version__ = '0.10.4'
